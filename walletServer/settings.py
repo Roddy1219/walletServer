34d0c1a67a80c9415ca5bd1bd172815bf9ac9e15
923401910@qq.com <923401910@qq.com>
2018-03-12 14:36:20 +0800

    修改tools目录名

--- walletServer/settings.py
+++ walletServer/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps/extends'))
-sys.path.insert(0, os.path.join(BASE_DIR, 'bitcoin_tools'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'utils'))
+
 
 
 try:
